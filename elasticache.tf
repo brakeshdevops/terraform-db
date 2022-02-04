6844184f06323ca7e22891efcd871ec72795730b
rakesh <bandarupallirakesh@gmail.com>
2022-02-04 16:26:53 +0530

    Shared library updated

---
+++ elasticache.tf
@@ -0,0 +1,48 @@
+resource "aws_elasticache_cluster" "redis" {
+  cluster_id = "redis-${var.ENV}"
+  engine = "redis"
+  node_type = "cache.t3.micro"
+  num_cache_nodes = 1
+  engine_version = "6.x"
+  port=6379
+  subnet_group_name =aws_elasticache_subnet_group.subnet-group.name
+  security_group_ids = [aws_security_group.redis.id]
+}
+resource "aws_elasticache_subnet_group" "subnet-group" {
+  name       = "redis-${var.ENV}"
+  subnet_ids = data.terraform_remote_state.vpc.outputs.PRIVATE_SUBNETS
+}
+resource "aws_security_group" "redis" {
+  name        = "sg_redis_${var.ENV}"
+  description = "sg_redis_${var.ENV}"
+  vpc_id = data.terraform_remote_state.vpc.outputs.VPC_ID
+
+  ingress {
+    description = "APP"
+    from_port   = 6379
+    to_port     = 6379
+    protocol    = "tcp"
+    cidr_blocks = data.terraform_remote_state.vpc.outputs.PRIVATE_SUBNET_CIDR
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "sg_redis_${var.ENV}"
+  }
+}
+output "test" {
+  value=aws_elasticache_cluster.redis
+}
+resource "aws_route53_record" "redis" {
+  name    = "redis-${var.ENV}.roboshop.internal"
+  type    = "CNAME"
+  ttl="300"
+  zone_id = data.terraform_remote_state.vpc.outputs.PRIVATE_HOSTED_ZONE_ID
+  records=[aws_elasticache_cluster.redis.cache_nodes[0].address]
+}
